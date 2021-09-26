8db2103f2f429ff10bdd334ccec7cb60b125c3e1
Anh Tran <anh.tran@zalora.com>
2021-09-27 01:25:19 +0700

    remove unused loading

--- Weather/Screens/BaseViewController/BaseViewController.swift
+++ Weather/Screens/BaseViewController/BaseViewController.swift
@@ -8,34 +8,14 @@
 import Foundation
 import UIKit
 class BaseViewController: UIViewController {
-    lazy var activityIndicationView = ActivityIndicatorView(style: .medium)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        activitySetUp()
     }
     
-    private func activitySetUp() {
-        activityIndicationView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(activityIndicationView)
-        NSLayoutConstraint.activate([
-        activityIndicationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        activityIndicationView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        activityIndicationView.heightAnchor.constraint(equalToConstant: 50),
-        activityIndicationView.widthAnchor.constraint(equalToConstant: 50.0)])
-    }
-    
-    func startLoading() {
-        activityIndicationView.isHidden = false
-        activityIndicationView.startAnimating()
-    }
-    
-    func finishLoading() {
-        activityIndicationView.stopAnimating()
-    }
-    
-    func showError(_ error: Error) {
-        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+    func showError(_ message: String) {
+        let alertController = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
+        alertController.view.accessibilityIdentifier = "alertController"
         let alertAction = UIAlertAction(title: "OK", style: .default) { [unowned self] _ in
             self.dismiss(animated: true, completion: nil)
         }
